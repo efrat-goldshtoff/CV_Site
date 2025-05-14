66e00784da80cc47f6e5b955e499cd39755e0341
efratGoldshtoff <eg0548449793@gmail.com>
2025-05-14 20:02:51 +0300

    add options

--- CVSite.API/Program.cs
+++ CVSite.API/Program.cs
@@ -1,11 +1,17 @@
+using CVSite.SERVICE;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.Configure<GitHubOptions>(builder.Configuration.GetSection(nameof(GitHubOptions)));
+builder.Services.AddGitHubIntegration(options => builder.Configuration.GetSection(nameof(GitHubOptions)).Bind(options));
 
 var app = builder.Build();
 
---
+++ CVSite.SERVICE/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CVSite.SERVICE
+{
+    public static class Extensions
+    {
+        public static void AddGitHubIntegration(this IServiceCollection services, Action<GitHubOptions> configurOptions)
+        {
+            services.Configure(configurOptions);
+            services.AddScoped<IGitHubService, GitHubService>();
+        }
+    }
+}
